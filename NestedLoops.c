01934eaa73c952869ff97d88ce841bc2729704a1
shabareenaa-annadurai <36302047+shabareenaa-annadurai@users.noreply.github.com>
2018-02-09 20:20:35 +0530

    Update and rename Untitled1.c to NestedLoops.c

--- NestedLoops.c
+++ NestedLoops.c
@@ -5,13 +5,13 @@ void main()
     int n,i,j,k;
     printf("enter no of rows:");
     scanf("%d",&n);
-    for(i=1;i<=n;i++)
+    for(i=1;i<=n;i++)       //for printing rows
     {
-        for(j=1;j<=n-i;j++)
+        for(j=1;j<=n-i;j++)     //for printing dots in rows
         {
             printf(".");
         }
-        for(k=1;k<=i;k++)
+        for(k=1;k<=i;k++)       //for printing numbers
             {
                 printf("%d",i);
             }
